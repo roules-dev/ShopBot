fc60445fcfdbfe23db9171f0639054d1ab359ec7
roulès <1jules2second@gmail.com>
2025-11-08 22:58:44 +0100

    Rename event from 'ready' to 'clientReady' (event ready is deprecated)

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -2,7 +2,7 @@ import { ActivityType, Client } from 'discord.js'
 import { PrettyLog } from '../utils/pretty-log'
 import { getSetting } from '../database/settings/settings-handler'
 
-export const name = 'ready'
+export const name = 'clientReady'
 export const once = true
 
 export async function execute(client: Client) {
