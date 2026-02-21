96bcbc8fa881c90771d813fc922f1c17464bf5e0
DragonJules <1jules2second@gmail.com>
2026-02-21 22:44:18 +0100

    got rid of deprecated code

--- src/features/settings/database/settings-handler.ts
+++ src/features/settings/database/settings-handler.ts
@@ -1,7 +1,6 @@
 import settings from '@/../data/settings.json' with { type: 'json' }
 import { Setting, Settings, SettingsDatabase } from '@/features/settings/database/settings-types.js'
 import { err, ok } from '@/lib/error-handling.js'
-import { setCurrentLocale } from '@/lib/localisation.js'
 import { EVENTS } from '@/middleware.js'
 
 const settingsDatabase = new SettingsDatabase(settings, "data/settings.json")
@@ -27,20 +26,7 @@ export async function setSetting(id: string, value: any) {
     settingsDatabase.data.set(id, updatedSetting)
 
     await settingsDatabase.save()
-    await onSettingUpdate(updatedSetting)
     EVENTS.emit("settingUpdated", id, updatedSetting)
 
     return ok(settingsDatabase.data.get(id)!)
-}
-
-export async function onSettingUpdate(setting: Setting) { // TODO this is horrible, should find a better way to do this, maybe with events (event bus ?)
-    switch (setting.id) {
-        case "language":
-            await setCurrentLocale(setting.value as string | undefined)  
-            break
-        case "activityMessage":
-        case "activityType":
-            // TODO: Update activity
-            break
-    }
 }
